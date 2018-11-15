b9f30a620d51c6409f68f103ccc2fe500454dc86
2iw31Zhv <ziweizhu95@gmail.com>
2018-11-15 16:31:27 -0500

    initial finish, but need to consider the resolution and possible bugs

--- BPM/fd_bpm.m
+++ BPM/fd_bpm.m
@@ -15,7 +15,7 @@ n1 = 1.46;
 n2 = 3.48;
 
 % effective propagation refractive index
-n_eff = 2.0;
+n_eff = 2.8;
 
 % wavelength of light in vacuum
 lambda0 = 1.55e-6; 
@@ -24,17 +24,17 @@ k0 = 2 * pi / lambda0;
 
 % define simulation parameters
 z0 = 0.0;
-z1 = 2e-4;
+z1 = 2e-5;
 x0 = -1e-5;
 x1 = 1e-5;
 
 wg_1_z0 = 0.0;
-wg_1_z1 = 1.5e-4;
+wg_1_z1 = 1.5e-5;
 wg_1_x0 = -5e-6;
 wg_1_x1 = -1e-6;
 
-wg_2_z0 = 5e-5;
-wg_2_z1 = 2e-4;
+wg_2_z0 = 5e-6;
+wg_2_z1 = 2e-5;
 wg_2_x0 = 1e-6;
 wg_2_x1 = 5e-6;
 
@@ -55,8 +55,8 @@ wg_2_z1 = wg_2_z1 * k0;
 wg_2_x0 = wg_2_x0 * k0;
 wg_2_x1 = wg_2_x1 * k0;
 
-grid_per_devlen = 16;
-grid_per_wavelen = 20;
+grid_per_devlen = 32;
+grid_per_wavelen = 100;
 
 dev_min_feature_len = min(abs(wg_1_x1 - wg_1_x0), abs(wg_2_x1 - wg_2_x0));
 
@@ -106,11 +106,9 @@ Dx_e = (0.5 * diag(ones(Nx-1,1), 1) - 0.5 * diag(ones(Nx-1, 1), -1)) / res_x;
 
 % initialize field
 Ey = zeros(Nz, Nx);
-Hx = zeros(Nz, Nx);
-Hz = zeros(Nz, Nx);
 
 % compute source at the first plane
-tau = 0.1 * (wg_1_x1 - wg_1_x0);
+tau = 0.5 * (wg_1_x1 - wg_1_x0);
 delay_pos =  0.5 * (wg_1_x1 + wg_1_x0);
 Generator = @(t)exp(-0.5 * ((t - delay_pos) ./ tau).^2);
 for i = 1 : Nx
@@ -119,6 +117,52 @@ for i = 1 : Nx
 end
 
 % iterate to update field
-for T = 1 : Nz
+for i = 1 : Nz-1
+    % extract and diagonalize constitutive tensor
+    mu_xx0 = diag(Mu_xx(i, :));
+    mu_zz0 = diag(Mu_zz(i, :));
+    eps_yy0 = diag(Eps_yy(i, :));
     
+    inverse_mu_zz0 = diag(Mu_zz(i,:).^(-1));
+    
+    A0 = mu_xx0 * Dx_h * inverse_mu_zz0 * Dx_e + mu_xx0 * eps_yy0...
+        - n_eff^2 * ones(Nx, Nx);
+    
+    mu_xx1 = diag(Mu_xx(i+1, :));
+    mu_zz1 = diag(Mu_zz(i+1, :));
+    eps_yy1 = diag(Eps_yy(i+1, :));
+    
+    inverse_mu_zz1 = diag(Mu_zz(i+1,:).^(-1));
+    
+    A1 = mu_xx1 * Dx_h * inverse_mu_zz1 * Dx_e + mu_xx1 * eps_yy1...
+        - n_eff^2 * ones(Nx, Nx);  
+    
+    left_hand_mat = ones(Nx, Nx) - 1j * res_z / 4.0 / n_eff * A1;
+    rhs = (ones(Nx, Nx) + 1j * res_z / 4.0 / n_eff * A0) * (Ey(i, :)');
+    
+    Ey(i+1, :) = (left_hand_mat \ rhs)';
+    x_array = x0 + res_x * ((1:Nx)-0.5);
+    z_array = z0 + res_z * ((1:Nz)-0.5);
+    
+    if (mod(i, 20) == 0)
+        clear gca;
+        imagesc(z_array, x_array, abs(Ey)');
+        hold on;
+        plot([wg_1_z0 wg_1_z0], [wg_1_x0 wg_1_x1], '-k');
+        hold on;
+        plot([wg_1_z0 wg_1_z1], [wg_1_x0 wg_1_x0], '-k');
+        hold on;
+        plot([wg_1_z1 wg_1_z1], [wg_1_x0 wg_1_x1], '-k');
+        hold on;
+        plot([wg_1_z0 wg_1_z1], [wg_1_x1 wg_1_x1], '-k');
+        hold on;
+        plot([wg_2_z0 wg_2_z0], [wg_2_x0 wg_2_x1], '-k');
+        hold on;
+        plot([wg_2_z0 wg_2_z1], [wg_2_x0 wg_2_x0], '-k');
+        hold on;
+        plot([wg_2_z1 wg_2_z1], [wg_2_x0 wg_2_x1], '-k');
+        hold on;
+        plot([wg_2_z0 wg_2_z1], [wg_2_x1 wg_2_x1], '-k');
+        pause(0.01);
+    end
 end
