839a337584adf3a58e39eda7d161d41d99d0f234
2iw31Zhv <ziweizhu95@gmail.com>
2018-12-13 14:46:48 -0500

    finish global s matrix

--- RCWA/rcwa3d.m
+++ RCWA/rcwa3d.m
@@ -22,8 +22,9 @@ Lx = 1.75; % cm
 Ly = 1.50; % cm
 
 % thickness
-d1 = 0.50; % cm
-d2 = 0.30; % cm
+d = zeros(2, 1);
+d(1) = 0.50; % cm
+d(2) = 0.30; % cm
 
 % hole size of layer 1
 w = 0.8 * Lx;
@@ -32,7 +33,7 @@ w = 0.8 * Lx;
 % define source
 lambda0 = 2.0; % cm
 k0  = 2.0 * pi / lambda0; % cm^-1
-theta_src = pi / 4.0; % arc
+theta_src = 0; % arc
 phi_src = pi / 4.0; % arc
 
 
@@ -70,9 +71,6 @@ for i  = 1 : Nx
     end
 end
 
-imagesc(ER(:, :, 1)');
-axis equal tight;
-
 % build device for layer 2
 ER(:, :, 2) = eps_r;
 UR(:, :, 2) = mu_r;
@@ -80,24 +78,24 @@ UR(:, :, 2) = mu_r;
 % calculate the corresponding convolution matrix;
 
 % number of harmonics
-P = ceil(7 * Lx / lambda0);
-if mod(P, 2) == 0
-    P = P + 1; 
+M = ceil(7 * Lx / lambda0);
+if mod(M, 2) == 0
+    M = M + 1; 
 end
     
-Q = ceil(7 * Ly / lambda0);
-if mod(Q, 2) == 0
-    Q = Q + 1;
+N = ceil(7 * Ly / lambda0);
+if mod(N, 2) == 0
+    N = N + 1;
 end
 
-ERC = zeros(P*Q, P*Q, 2);
-URC = zeros(P*Q, P*Q, 2);
+ERC = zeros(M*N, M*N, 2);
+URC = zeros(M*N, M*N, 2);
 
-ERC(:, :, 1) = convmat(ER(:, :, 1), P, Q);
-ERC(:, :, 2) = convmat(ER(:, :, 2), P, Q);
+ERC(:, :, 1) = convmat(ER(:, :, 1), M, N);
+ERC(:, :, 2) = convmat(ER(:, :, 2), M, N);
 
-URC(:, :, 1) = convmat(UR(:, :, 1), P, Q);
-URC(:, :, 2) = convmat(UR(:, :, 2), P, Q);
+URC(:, :, 1) = convmat(UR(:, :, 1), M, N);
+URC(:, :, 2) = convmat(UR(:, :, 2), M, N);
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -106,8 +104,8 @@ URC(:, :, 2) = convmat(UR(:, :, 2), P, Q);
 
 % setup wave number matrices
 wavenumber = struct;
-wavenumber.M = floor(P / 2);
-wavenumber.N = floor(Q / 2);
+wavenumber.M = floor(M / 2);
+wavenumber.N = floor(N / 2);
 wavenumber.m = -wavenumber.M : wavenumber.M;
 wavenumber.n = -wavenumber.N : wavenumber.N;
 
@@ -124,8 +122,106 @@ KY = diag(sparse(Kyn(:)));
 KZ_ref = diag(sparse(Kz_ref(:)));
 KZ_trn = diag(sparse(Kz_trn(:)));
 
-% compute eigen modes of free space
+% compute eigen modes of free space (analyze gap medium)
+% calculate W0, V0
+
+W0 = sparse(eye(2*M*N));
+Q0 = [KX * KY, eye(M*N) - KX * KX;
+    KY * KY - eye(M*N), - KX * KY];
+Kz0 = sqrt(1.0 - Kxm.^2 - Kyn.^2)';
+KZ0 = diag(sparse(Kz0(:)));
+LAM0 = [1j * KZ0, zeros(M*N);
+    zeros(M*N), 1j * KZ0];
+V0 = Q0 / LAM0;
 
 
+% initialize global s matrix
+SG = struct;
+SG.S11 = zeros(2*M*N);
+SG.S12 = eye(2*M*N);
+SG.S21 = eye(2*M*N);
+SG.S22 = zeros(2*M*N);
+
+% main loop through all layers
+for i = 1 : 2
+    % build eigen value problem
+    ERi = ERC(:, :, i);
+    URi = URC(:, :, i);
+    
+    Pi = [KX / ERi * KY, URi - KX / ERi * KX;
+        KY / ERi * KY - URi, -KY / ERi * KX];
+    Qi = [KX / URi * KY, ERi - KX / URi * KX;
+        KY / URi * KY - ERi, -KY / URi * KX];
+    OMEGA2 = Pi * Qi;
+    
+    [Wi, LAMi] = eig(OMEGA2);
+    LAMi = sqrt(LAMi);
+    Vi = Qi * Wi / LAMi;
+    
+    % calculate layer scatter matrix
+    Ai0 = Wi \ W0 + Vi \ V0;
+    Bi0 = Wi \ W0 - Vi \ V0;
+    Xi = exp(-LAMi * k0 * d(i));
+    
+    
+    S = struct;
+    S.S11 = (Ai0 - Xi * Bi0 / Ai0 * Xi * Bi0)...
+        \ (Xi * (Bi0 / Ai0) * Xi * Ai0 - Bi0);
+    S.S12 = (Ai0 - Xi * Bi0 / Ai0 * Xi * Bi0)...
+        \ Xi * (Ai0 - Bi0 / Ai0 * Bi0);
+    S.S21 = S.S12;
+    S.S22 = S.S11;
+    
+    % update global scatter matrix
+    SG = redheffer_star_product_expand2(SG, S);
+    
+end
+
+% compute reflection side s matrix
+Qref = 1.0 / mu_ref * [KX * KY, mu_ref * eps_ref * eye(M*N) - KX * KX;
+    KY * KY - mu_ref * eps_ref * eye(M*N), -KY * KX];
+Wref = eye(2*M*N);
+LAMref = [-1j * KZ_ref, zeros(M*N);
+    zeros(M*N), -1j * KZ_ref];
+Vref = Qref / LAMref;
+
+Aref = W0 \ Wref + V0 \ Vref;
+Bref = W0 \ Wref - V0 \ Vref;
+
+SR = struct;
+SR.S11 = - Aref \ Bref;
+SR.S12 = 2.0 * inv(Aref);
+SR.S21 = 0.5 * (Aref - Bref / Aref * Bref);
+SR.S22 = Bref / Aref;
+
+% compute transmission side s matrix
+Qtrn = 1.0 / mu_trn * [KX * KY, mu_trn * eps_trn * eye(M*N) - KX * KX;
+    KY * KY - mu_trn * eps_trn * eye(M*N), -KY * KX];
+Wtrn = eye(2*M*N);
+LAMtrn = [1j * KZ_trn, zeros(M*N);
+    zeros(M*N), 1j * KZ_trn];
+Vtrn = Qtrn / LAMtrn;
+
+Atrn = W0 \ Wtrn + V0 \ Vtrn;
+Btrn = W0 \ Wtrn - V0 \ Vtrn;
+
+ST = struct;
+ST.S11 = Btrn / Atrn;
+ST.S12 = 0.5 * (Atrn - Btrn / Atrn * Btrn);
+ST.S21 = 2.0 * inv(Atrn);
+ST.S22 = -Atrn \ Btrn;
+
+% update global s matrix
+SG = redheffer_star_product_expand2(SR, SG);
+SG = redheffer_star_product_expand2(SG, ST);
+
+% compute source
+imagesc(abs(SG.S12));
+
+% solve for reflected & transmitted field
+
+% calculate diffraction efficiencies
+
+% verify conservation
 
 
